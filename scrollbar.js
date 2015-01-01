c99259b218d3296f297e9cf9cf96ceec75976411
cycttn <BryanCua@Bryan-Cuas-MBP.local>
2015-01-01 00:18:07 -0800

    Reposition implementing drag on init Added argument to trigger ::dragY; ::dragX Triggered ::dragY; ::dragX when update() is called

--- scrollbar.js
+++ scrollbar.js
@@ -23,12 +23,12 @@
    var defaults = {
        h: 1,
        v: 1,
-       autoHide: 1,
-       grow: 0,
+       autoHide: 0,
+       grow: 1,
        growBy: 5
    };
    
-   var dragJs = 'drag.js'; //location of drag.js
+   //var dragJs = 'drag.js'; //location of drag.js
    
    /**
     * Scrollbar
@@ -43,14 +43,16 @@
         
         if( this.o.h ){
             this.$h = $horz.clone();
-            this.$p.append(this.$h);                                 
+            this.$p.prepend(this.$h);                                 
         }
         
         if( this.o.v ){
             this.$v = $vert.clone();
-            this.$p.append(this.$v);                                                
+            this.$p.prepend(this.$v);                                                
         }
 
+        //allow drag if file exists
+		if( $.fn.drag ) implementDrag.call(this);
         this.reset(); //set height and width
 
         //add event listener to parent
@@ -61,15 +63,10 @@
         }
         
         if( this.o.grow ){
-            var growBy = this.o.growBy; 
-            if( this.$v ) this.$v.mouseover(function(){ $(this).css('width', '+=' + growBy); }).mouseout(function(){ $(this).css('width', '-=' + growBy); });
-            if( this.$h ) this.$h.mouseover(function(){ $(this).css('height', '+=' + growBy); }).mouseout(function(){ $(this).css('height', '-=' + growBy); });
+            var gb = this.o.growBy;
+            if( this.$v ) this.$v.mouseover(function(){ $(this).css('width', '+=' + gb); }).mouseout(function(){ $(this).css('width', '-=' + gb); });
+            if( this.$h ) this.$h.mouseover(function(){ $(this).css('height', '+=' + gb); }).mouseout(function(){ $(this).css('height', '-=' + gb); });
         }
-
-        //allow drag if file exists
-        //$.loadJs(dragJs, $.proxy(implementDrag, this) );   
-        implementDrag.call(this);
-        
     };
 
     function implementDrag(){    
@@ -90,20 +87,21 @@
 
     function dragStart(){ this.dragging = true; };
     function dragStop(){ this.dragging = false; };
+	
     function draggingX(ev, dx, dy){ 
         var pdx = dx / (this.$p.width() - this.w); 
-        this.$t.trigger('scrollbar::dragX', [pdx]); 
+        this.$t.trigger('scrollbar::dragX', [pdx, dx]); 
     };
     
     function draggingY(ev, dx, dy){ 
         var pdy = dy / (this.$p.height() - this.h); 
-        this.$t.trigger('scrollbar::dragY', [pdy]); 
+        this.$t.trigger('scrollbar::dragY', [pdy, dy]); 
     };
-
+	
     /**
      * Shows the scrollbar
      */
-    $.scrollbar.prototype.show = function(){
+    $.scrollbar.prototype.show = function(){        
         if( this.o.v ) this.$v.animate({opacity: 1}, 50);
         if( this.o.h ) this.$h.animate({opacity: 1}, 50);
         this.$t.trigger('scrollbar::show', [this.$v, this.$h]);
@@ -128,6 +126,7 @@
             if( hi > h ){
                 this.h = h/hi*h;
                 this.$v.css('height', this.h);
+                if( $.fn.drag ) this.$v.drag().o.max = this.$p.height()-this.h;
             }
         }
         
@@ -136,6 +135,7 @@
             if( wi > w ){
                 this.w = w/wi*w;
                 this.$h.css('width', this.w);
+                if( $.fn.drag ) this.$h.drag().o.max = this.$p.width()-this.w;
             }
         }     
     };
@@ -145,18 +145,20 @@
      * @param {double} x - horizontal % (integer from 0-1)
      * @param {double} y - vertical % (integer from 0-1)
      */
-    $.scrollbar.prototype.update = function(x, y){        
+    $.scrollbar.prototype.update = function(x, y){   
         if( y < 0 ) y=0; if( x < 0 ) x=0;
         if( y > 1 ) y=1; if( x > 1 ) x=1;
         
         if( this.o.v && this.$v ){            
             var t = (this.$p.height() - this.h)*y;
             this.$v.css({ top: t });
+	        this.$t.trigger('scrollbar::dragY', [y,t]); 			
         }
         
         if( this.o.h && this.$h ){
             var l = (this.$p.width()-this.w)*x;
             this.$h.css({left: l});
+	        this.$t.trigger('scrollbar::dragX', [x,l]); 			
         }        
     };
 
